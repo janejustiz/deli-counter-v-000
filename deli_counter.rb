fa297f34bbdc5bcd485cd3a30a05b5bca5b20070
Jane Justiz <janejustiz@gmail.com>
2018-06-14 04:26:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,7 @@ def line(array)
   if array.length==0
     puts "The line is currently empty."
   else
-    counter = 1
-    list = []
-    array.each do |x|
-      list << "#{counter}. #{x}"
-      counter+=1
-    end
-    list.join(" ")
-    puts "The line is currently: #{list}"
+    puts "The line is currently: #{array.each}"
   end
 end
 
