2cb2ec9048d9417cdc167d44388709153aa62c13
Jane Justiz <janejustiz@gmail.com>
2018-06-14 04:09:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli=[]
+
+def line(array)
+  counter = 1
+  array.each do |x|
+    list = []
+    list << "#{counter}. x"
+  end
+  list.join(" ")
+  puts "The line is currently: #{list}"
+end
+
+def take_a_number(line,name)
+  array << name
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving (array)
+  if x.length == 0
+    "There is nobody waiting to be served!"
+  else
+    puts
+    x.shift
